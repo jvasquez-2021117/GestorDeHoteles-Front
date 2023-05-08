db5f39c6c2b195b100927f85b8a11f75d505bdb4
pvasquez-2018044 <pvasquez-2018044@kinal.edu.gt>
2023-05-08 11:54:09 -0600

    Se creo la vista de hoteles

---
+++ GestorHoteles (Front-end)/src/components/Options/OptionHotels.jsx
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Hotels } from '../Hotels/Hotels'
+
+export const OptionHotels = () => {
+    const [hotels, setHotels] = useState([{}])
+
+    const getHotels = async() => {
+        try {
+            const { data } = await axios('http://localhost:3200/hotel/getHotel')
+            setHotels(data.hotel)
+        }catch(e){
+            console.log(e);
+        }        
+    }
+
+    useEffect(() => getHotels, []);
+    return (
+        <>
+        {
+            hotels.map(({_id, name, description, address, qualification, rooms}, index) =>{
+            <div key={index}>
+                <Hotels
+                name={name}
+                description={description}
+                address={address}
+                qualification={qualification}
+                rooms={rooms}
+                ></Hotels>
+            </div>    
+            })
+        }
+
+        </>
+    )
+}
--- GestorHoteles (Front-end)/src/pages/HomePage.jsx
+++ GestorHoteles (Front-end)/src/pages/HomePage.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import { OptionHome } from '../components/OptionHome'
 import { Sidebar } from '../components/Sidebar'
-import { OptionsRooms } from '../components/Options/OptionsRooms'
-
 
 export const HomePage = () => {
     return (
         <>
-            <div className="container t">
-                <div className="row flex-lg-nowrap">
-                    <div className="col-12 col-lg-3 mb-3" >
-                        <div className="card">
+            <div class="container t">
+                <div class="row flex-lg-nowrap">
+                    <div class="col-12 col-lg-3 mb-3" >
+                        <div class="card">
                             <Sidebar></Sidebar>
                         </div>
                     </div>
-                    <div className="col">
-                        <div className="row">
-                            <div className="col mb-3">
-                                <OptionsRooms>
-                                </OptionsRooms>
+                    <div class="col">
+                        <div class="row">
+                            <div class="col mb-3">
                                 <OptionHome />
                                 <OptionHome />
                             </div>
